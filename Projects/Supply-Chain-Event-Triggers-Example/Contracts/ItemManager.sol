62359f04152c3555fca0e01d507512c3983a4352
mindeleven <mindeleven@posteo.de>
2022-04-12 11:47:17 +0200

    Adding functionality to the ItemManager contract

--- Projects/Supply-Chain-Event-Triggers-Example/Contracts/ItemManager.sol
+++ Projects/Supply-Chain-Event-Triggers-Example/Contracts/ItemManager.sol
@@ -18,14 +18,21 @@ contract ItemManager {
     uint itemIndex;
 
     function createItem(string memory _identifier, uint _itemPrice) public {
-
+        items[itemIndex]._identifier = _identifier;
+        items[itemIndex]._itemPrice = _itemPrice;
+        items[itemIndex]._state = SupplyChainState.Created;
+        itemIndex++;
     }
 
-    function triggerPayment() public {
-
+    function triggerPayment(uint _itemIndex) public payable {
+        require(items[_itemIndex]._itemPrice == msg.value, "Only full payments accepted");
+        require(items[_itemIndex]._state == SupplyChainState.Created, "Item is further in the chain");
+        
+        items[_itemIndex]._state = SupplyChainState.Paid;
     }
 
-    function triggerDelivery() public {
-
+    function triggerDelivery(uint _itemIndex) public {
+        require(items[_itemIndex]._state == SupplyChainState.Paid, "Item is further in the chain");
+        items[_itemIndex]._state = SupplyChainState.Delivered;
     }
 }
