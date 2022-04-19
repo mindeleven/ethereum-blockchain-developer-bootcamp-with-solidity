8c78b6c02465acca7e1af904938228083c54261b
mindeleven <mindeleven@posteo.de>
2022-04-19 10:01:50 +0200

    A bit of refactoring, moving each contract to its own file

--- Projects/Supply-Chain-Event-Triggers-Example/Contracts/ItemManager.sol
+++ Projects/Supply-Chain-Event-Triggers-Example/Contracts/ItemManager.sol
@@ -2,58 +2,8 @@
 
 pragma solidity ^0.7.0;
 
-contract Ownable {
-    address public _owner;
-
-    constructor () {
-        _owner = msg.sender;
-    }
-
-    /**
-    * @dev Throws if called by any account other than the owner.
-    */
-    modifier onlyOwner() {
-        require(isOwner(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-    * @dev Returns true if the caller is the current owner.
-    */
-    function isOwner() public view returns (bool) {
-        return (msg.sender == _owner);
-    }
-}
-
-
-contract Item {
-    uint public priceInWei;
-    uint public pricePaid;
-    uint public index;
-
-    ItemManager parentContract;
-
-    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) {
-        priceInWei = _priceInWei;
-        index = _index;
-        parentContract = _parentContract;
-    }
-
-    receive() external payable {
-        require(pricePaid == 0, "Item is paid already");
-        require(priceInWei == msg.value, "Only full payments allowed");
-        pricePaid += msg.value;
-        // it's crucially important to get return value back with low level call function
-        // call function gives two return values
-        // (1) boolean that tells if it was successful
-        // (2) possible return values
-        (bool success, ) = address(parentContract).call{value:msg.value}(abi.encodeWithSignature("triggerPayment(uint256)", index));
-        require(success, "The transaction wasn't successful, canceling");
-    }
-
-    // remix needs fallback function to interact with smart contract
-    fallback () external {}
-}
+import "./Ownable.sol";
+import "./Item.sol";
 
 contract ItemManager is Ownable {
 
